67e1fc4af3c0a0375acc5ae4a64027d92718769c
Joshua Serwatien <josh@greatideafactory.com>
2024-01-07 02:49:45 -0600

    Added schedule

---
+++ MCON451.Data/Code/ScheduleFactory.cs
@@ -0,0 +1,40 @@
+﻿using MCON451.Entity;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCON451.Data.Code
+{
+    public class ScheduleFactory
+    {
+        public static List<StringIntDoubleDateEntity> GetScheduleData(UserEntity currentUser)
+        {
+            List<StringIntDoubleDateEntity> returnData = new();
+            Random newRandom = new();
+            string strSQL = "EXEC dbo.Schedule_GetData";
+            DataSet ds = new();
+            try
+            {
+                ds = DataFactory.GetDataSet(strSQL, "ScheduleData");
+                foreach(DataRow newRow in ds.Tables[0].Rows)
+                {
+                    StringIntDoubleDateEntity newItem = new();
+                    newItem.IntValue = Convert.ToInt32(newRow["MonthlySalesKey"]);
+                    newItem.DateValue = Convert.ToDateTime(newRow["SalesMonth"] +"/" + newRandom.Next(1,28) + "/" + newRow["SalesYear"]);
+                    newItem.StringValue = newRow["FirstName"] + " " + newRow["LastName"] + " generated " +
+                            Convert.ToDecimal(newRow["SalesAmount"]).ToString("c") + " in sales for " + newRow["StoreName"] + " (" +
+                            newRow["Campaign"] + ")";
+                    returnData.Add(newItem);
+                }
+                return returnData;
+            }
+            catch (System.Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
